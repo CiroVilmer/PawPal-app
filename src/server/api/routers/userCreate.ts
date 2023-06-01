26840d05f0e674cd4e37fc90de4ca26b37d26de6
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-06-01 13:49:00 -0300

    updated db and created router for user create

---
+++ src/server/api/routers/userCreate.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+import { prisma } from "~/server/db";
+//min 36:25
+export const userRouter = createTRPCRouter({
+    createUser: publicProcedure.input(
+        z.object({
+            name: z.string(),
+            dni: z.number(),
+            email: z.string(),
+            
+        })
+    )
+    .mutation(({ input, ctx }) => {
+        const user = prisma.user.create({
+        data:{
+            name: input.name,
+            dni: input.dni,
+            email: input.email,
+
+
+        }    
+        });
+        return user
+    })
+
+});
