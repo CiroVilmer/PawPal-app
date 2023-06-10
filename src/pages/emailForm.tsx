85f751d6e0d34f81c1634396c132d7c22300d8c9
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-06-10 01:30:54 -0300

    startedPasswordRecovery

---
+++ src/pages/emailForm.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from 'next/link';
+
+export function emailForm(){
+
+    <div className = "flex justify-start max-w-md mt-20 ml-28">
+        <form className="border-solid border border-gray rounded-md shadow-md p-8 ">
+                
+
+        <h1 className="flex justify-center text-5xl font-bold py-1 text-black px-12 mb-3">
+            Paw<span className="text-[rgb(252,119,80,100%)]">Pal</span>
+        </h1>
+        <div className="flex justify-center mt-8 mb-5">
+            <button className="text-md text-gray-800">         
+            <Link href = "/">Volver al <span className = "text-orange-600 hover:underline">Log in</span></Link>   
+            </button>
+        </div>
+        <label className="flex justify-center text-center font-normal text-lg mb-10">
+            Lorem ipsum dolor sit amet consectetur. Quam quam imperdiet cursus fusce aliquam.  
+        </label>
+        
+        <label className = "text-md px-1 font-semibold"> 
+            Correo electronico
+        </label>
+        <input
+            type="string"
+            id="email"
+            className="block w-full border border-gray-300 rounded-md py-2 px-2 font-normal mb-12"
+            placeholder="ejemplo@gmail.com"
+            required
+            //{...formik.getFieldProps('email')}
+            />
+        {/* {formik.errors.email && formik.touched.email ? <div className = "text-red-500 text-xs">{formik.errors.email}</div> : null} */}
+        </form>
+    </div>
+   
+}
--- src/pages/forgotPassword.tsx
+++ src/pages/forgotPassword.tsx
@@ -3,7 +3,12 @@ import Link from 'next/link';
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import { api } from "~/utils/api";
-import { Stepper,Button, Group } from '@mantine/core';
+import { Stepper,Button, Group, ColorPicker } from '@mantine/core';
+import { IconSquareRounded } from '@tabler/icons-react';
+import {AiOutlineArrowRight, AiOutlineArrowLeft} from 'react-icons/ai'
+import { multiStepForm } from './multiStepForm';
+import { relative } from 'path';
+import { emailForm } from './emailForm';
 
     
 
@@ -25,55 +30,38 @@ export function forgotPassword():JSX.Element
     };
     const shouldAllowSelectStep = (step: number) => highestStepVisited >= step && active !== step;
 
-
-
+    const {steps, currentStepIndex, back, next, isFirstStep, isLastStep} = multiStepForm([])
     
     return( 
         
         //<div className="container max-w-md mr-auto absolute mt-15 left-28">
         <div className = "flex justify-start max-w-md mt-20 ml-28">        
             
-            <div className="border-solid border border-gray rounded-md shadow-md p-8 ">
-                <div>
-                    <h1 className="flex justify-center text-5xl font-bold py-1 text-black px-12 mb-3">
-                        Paw<span className="text-[rgb(252,119,80,100%)]">Pal</span>
-                    </h1>
-                    <div className="flex justify-center mt-8 mb-5">
-                        <button className="text-md text-gray-800">         
-                        <Link href = "/">Volver al <span className = "text-orange-600 hover:underline">Log in</span></Link>   
-                        </button>
-                    </div>
-                    <label className="flex justify-center text-center font-normal text-lg mb-10">
-                        Lorem ipsum dolor sit amet consectetur. Quam quam imperdiet cursus fusce aliquam.  
-                    </label>
-                    <form>
-                        <label className = "text-md px-1 font-semibold"> 
-                        Correo electronico
-                        </label>
-                        <input
-                        type="string"
-                        id="email"
-                        className="block w-full border border-gray-300 rounded-md py-2 px-2 font-normal mb-12"
-                        placeholder="ejemplo@gmail.com"
-                        required
-                        //{...formik.getFieldProps('email')}
-                        />
-                        {/* {formik.errors.email && formik.touched.email ? <div className = "text-red-500 text-xs">{formik.errors.email}</div> : null} */}
-
-                    </form>    
-
-                    <Stepper size="sm" active={0}>
-                        <Stepper.Step label="Step 1" description="Create an account" />
-                        <Stepper.Step label="Step 2" description="Verify email" />
-                    </Stepper>
-
+      
+            <Button className="w-full bg-orange-500 text-white rounded-xl py-2 hover:bg-orange-600" variant='normal'>
+                <Link href="/emailForm">formulario email</Link>
+            </Button>
                     
-                </div> 
+
+                <Stepper size="xs" active={currentStepIndex} color='orange'>
+                    <Stepper.Step label="Paso 1" description="Ingresar mail" />
+                    <Stepper.Step label="Paso 2" description="Código de seguridad" />
+                    <Stepper.Step label="Paso 3" description="Reestablecer contraseña" />
+                </Stepper>
+
+                <div className='pt-5 pb-3'>
+                    {!isFirstStep && 
+                    <Button type="button" variant="light" color='orange' leftIcon={<AiOutlineArrowLeft></AiOutlineArrowLeft>} style={{color:"orange"}} onClick={back}>
+                    Back
+                    </Button>}
+                    <Button type='button' variant="light" color='orange' rightIcon={<AiOutlineArrowRight></AiOutlineArrowRight>} className='text-orange-600 relative ' onClick={next}>{isLastStep ? "Reestablecer contraseña":"Next step"}</Button>
+                </div>  
+               
                     
                 
                 
                            
-            </div>
+            
         </div>
         
         
