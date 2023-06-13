a43116482778797afa861da40ea6ac8882d8d8b3
FacuSalco <facusalco26@gmail.com>
2023-06-13 14:20:26 -0300

    Minor changes in the map

--- src/pages/mapa.tsx
+++ src/pages/mapa.tsx
@@ -8,17 +8,25 @@ const MapComponent: React.FC = () => {
   useEffect(() => {
     const map = L.map('map').setView([-34.5695195, -58.4468003], 12);
 
-    // L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    //   attribution:
-    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    //   subdomains: 'abcd',
-    //   maxZoom: 20,
+    //ESTE DE ABAJO ES SIMPLE Y LINDO PERO NO TIENE LUGARES
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd', 
+      maxZoom: 20,
+    }).addTo(map);
+
+    //ESTE TIENE LUGARES PERO NO ES LINDO
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   maxZoom: 19,
+    //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     // }).addTo(map);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    //ESTE TIENE LUGARES Y ESTA LINDO
+    // L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+    //   maxZoom: 20,
+    //   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    // }).addTo(map);
 
     return () => {
       map.remove();
