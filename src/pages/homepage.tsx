c70c3ce96e672bfeb5e3b9e0c804802b4196397b
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-06-23 13:29:33 -0300

    Update homepage.tsx

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,18 +1,33 @@
 import { getSession } from "next-auth/react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useState } from "react";
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
 import React from "react";
+import { type } from "os";
+import { Session } from "next-auth";
+
+type User = {
+    username: string;    
+}
+
 
 export function HomePage(): JSX.Element {
 
-    return (
-        <div>
-            <h1>Home Page - LoggedIn</h1>
-            <button onClick={() => signOut()}>Sign Out</button>
-        </div>
+    const {data: session, status} = useSession();
 
-    )
+    if (status === "authenticated") {
+        return (
+            <div>
+                <h1>Home Page - LoggedIn as {session.user.name}</h1>
+                <button onClick={() => signOut()}>Sign Out</button>
+            </div>
+        )
+    }else{
+        return (
+            <h1>Not Logged in</h1>
+        )
+    }
+
 
 }
 
