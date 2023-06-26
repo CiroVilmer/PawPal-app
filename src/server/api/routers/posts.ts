bf95fb40b531c816e3bf0c6a06a20d95de6e22d5
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-06-26 10:31:56 -0300

    started post creation

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from "~/server/api/trpc";
+
+import { prisma } from "~/server/db";
+
+export const postRouter = createTRPCRouter({
+    createPost: protectedProcedure.input(
+        z.object({
+            title: z.string(),
+            location: z.string(),
+        })
+    )
+    .mutation(async ({ input, ctx }) => {
+
+        const post = await ctx.prisma.post.create({
+            data: {
+                title: input.title,
+                location: input.location,
+                author:{ connect: { id: ctx.session.userId } },
+
+
+            }
+        });
+        return post
+
+    }),
+
+});
