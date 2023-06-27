b6ca213930b71f44351c6858681bf07b96525601
Luciano Manuel Del Arco <47138072@est.ort.edu.ar>
2023-06-27 14:27:34 -0300

    makingMapResponsive

--- src/pages/mapa.tsx
+++ src/pages/mapa.tsx
@@ -3,12 +3,19 @@ import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
 import { Chip, Group, Switch } from '@mantine/core';
 import { FaSearchLocation } from 'react-icons/fa';
+import {MdPets} from 'react-icons/md';
+import {BsScissors, BsHouseHeartFill} from 'react-icons/bs';
+import {BiBone} from 'react-icons/bi';
+import {GiDogHouse} from 'react-icons/gi';
+import {ReactNode} from 'react';
 
 const LeafletMap = dynamic(() => import('./Map/LeafletMap'), { ssr: false });
 
 interface ChipProps {
   num: string;
   servicio: string;
+  icono: ReactNode;
+  
 
 }
 
@@ -19,16 +26,17 @@ const MapComponent: React.FC = () => {
     console.log('Realizar búsqueda:', searchValue);
   };
 
-  const Chips: React.FC<ChipProps> = ({num, servicio}) => (
-    <Chip value={num} color='orange' variant='outline' className=''>{servicio}</Chip>
+  const Chips: React.FC<ChipProps> = ({num, servicio, icono}) => (
+    <Chip value={num} color='orange' variant='outline' className=''><i className='pr-2'>{icono}</i>{servicio}</Chip>
 
   );
 
   return (
     <div className="relative h-screen">
-      <div className="flex flex-col md:flex-row absolute flex-grow gap-8 md:left-20 z-20 items-center mt-5">
-        <div className="relative group">
-          <button
+      <div className="relative z-20 flex justify-center md:flex-grow md:gap-8 md:flex-row md:left-20 ">
+        <div className='flex items-center'>
+          <div className="absolute group flex items-end">
+            <button
               onClick={handleSearch}
               className="absolute top-0 right-0 flex items-center justify-center h-full w-9 bg-orange-400 text-white rounded-full"
             >
@@ -41,7 +49,8 @@ const MapComponent: React.FC = () => {
               placeholder="Buscar..."
               className="px-4 w-2 py-1 rounded-full border-2 focus:w-56 outline-none focus:border-orange-400 group-hover:w-56 transition-all duration-1000"
             />
-          
+            
+          </div>
         </div>
         {/* <button onClick={handleSearch} className="px-4 py-1 bg-orange-400 text-white rounded ml-2">
           Buscar
@@ -49,16 +58,18 @@ const MapComponent: React.FC = () => {
         
       
         <Chip.Group multiple>
-          <div className='flex flex-col md:flex-row w-72 overflow-x-scroll md:overflow-x-hidden lg:w-auto gap-4 overflow-hidden'>
-            <Chips num='1' servicio='Veterinaria'/>
-            <Chips num='2' servicio='Peluquería'/>
-            <Chips num='3' servicio='Refugio'/>
-            <Chips num='4' servicio='PetShop'/>
-            <Chips num='5' servicio='Guardería'/>
-            <Chips num='6' servicio='Peluquería'/>
-            <Chips num='7' servicio='Refugio'/>
-            <Chips num='8' servicio='PetShop'/>
-            <Chips num='9' servicio='Guardería'/>
+          <div className='flex flex-row mx-5 mt-5 pl-5 w-84  lg:w-auto gap-4 overflow-x-auto'>
+            <Chips num='1' icono={<MdPets/>} servicio='Veterinaria'/>
+            <Chips num='2' icono={<BsScissors/>} servicio='Peluquería'/>
+            <Chips num='3' icono={<GiDogHouse/>} servicio='Refugio'/>
+            <Chips num='4' icono={<BiBone/>} servicio='PetShop'/>
+            <Chips num='5' icono={<BsHouseHeartFill/>} servicio='Guardería'/>
+            <Chips num='6' icono={<MdPets/>} servicio='Veterinaria'/>
+            <Chips num='7' icono={<BsScissors/>} servicio='Peluquería'/>
+            <Chips num='8' icono={<GiDogHouse/>} servicio='Refugio'/>
+            <Chips num='9' icono={<BiBone/>} servicio='PetShop'/>
+            <Chips num='10' icono={<BsHouseHeartFill/>} servicio='Guardería'/>
+            
             
 
             {/* <button className="flex justify-end items-center pr-3 py-1 w-16 h-8 border-none bg-slate-100 text-black  rounded-full "></button> */}
