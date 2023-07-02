bf917323511f3fb9578589cc50d02dd8fd8df40f
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-07-02 00:01:46 -0300

    mediaQuery implemented

--- src/pages/Components/formWrapper.tsx
+++ src/pages/Components/formWrapper.tsx
@@ -1,21 +1,27 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
+import { useMediaQuery } from "@mantine/hooks"; 
+
 
 interface FormWrapperProps {
   children: ReactNode;
 }
 
 const FormWrapper: React.FC<FormWrapperProps> = ({ children }) => {
-  return (
-    <div>
 
-        <div className='z-20 absolute bottom-[0%] right-[0%] w-24 h-24'><img src="/logoPawPal.png" alt="logo" /> </div>
-        {/* <img src='/Group-2.png' className="lg:absolute lg:inset-0 lg:scale-100 bg-cover lg:h-screen lg:w-screen saturate-150 top-[0%]  z-0 lg:rotate-0 -rotate-90" alt="background"></img> */}
-        <div className="z-30 bg-orange-400 saturate-150 flex h-screen items-end md:items-center justify-center lg:justify-start">
-            <div className=' z-10 md:absolute md:shadow-gray-700 md:right-[0%] md:bg-white md:bg-cover md:shadow-2xl md:-inset-x-20 md:rounded-r-full md:w-screen md:h-screen'></div>
+    const largeScreen = useMediaQuery("(min-width: 1100px)");
+        
+
+  return (
+    <div className={ largeScreen ? "flex w-full h-screen justify-start" : "flex h-full w-full items-center justify-center"}>
+
+        {/* <div className='z-20 absolute bottom-[0%] right-[0%] w-24 h-24'><img src="/logoPawPal.png" alt="logo" /> </div> */}
+        <img src='/Group-2.png' className={ largeScreen ? "flex bg-cover h-screen w-screen absolute saturate-100 z-0" : "hidden" } alt="background"></img>
+        {/* <div className="z-30 bg-orange-400 saturate-150 flex h-screen items-end md:items-center justify-center lg:justify-start">
+            <div className=' z-10 md:absolute md:shadow-gray-700 md:right-[0%] md:bg-white md:bg-cover md:shadow-2xl md:-inset-x-20 md:rounded-r-full md:w-screen md:h-screen'></div> */}
 
             <div className="md:flex md:flex-row-reverse md:items-center md:gap-64">
-                <img src="/perrito-gatito.png" alt="dog" className='z-20 lg:drop-shadow-md w-80 h-80 lg:mr-44'/>
+                <img src="/perrito-gatito.png" alt="dog" className={ largeScreen ? 'z-20 drop-shadow-md w-80 h-80 ' : 'hidden'}/>
             
                 <div className="z-20 shadow-inner md:border-solid md:border md:shadow-md rounded-t-2xl mb-15 lg:ml-44 md:rounded-xl p-8 bg-white ">
                     <div className="flex justify-center font-bold py-1 text-xl mb-4">
@@ -32,7 +38,7 @@ const FormWrapper: React.FC<FormWrapperProps> = ({ children }) => {
                 </div>
             </div>
         </div>
-    </div>
+    
   );
 };
 
--- src/pages/mapa.tsx
+++ src/pages/mapa.tsx
@@ -9,6 +9,7 @@ import { BiBone } from 'react-icons/bi';
 import { GiDogHouse } from 'react-icons/gi';
 import { ReactNode } from 'react';
 import EmailForm from './CambiarContraseña/emailForm';
+import { useMediaQuery } from "@mantine/hooks"; 
 
 const LeafletMap = dynamic(() => import('./Map/LeafletMap'), { ssr: false });
 
@@ -17,6 +18,7 @@ interface ChipProps {
   servicio: string;
   icono: ReactNode;
 }
+
 
 const MapComponent: React.FC = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -32,61 +34,65 @@ const MapComponent: React.FC = () => {
     </Chip>
   );
 
+  const largeScreen = useMediaQuery("(min-width: 1010px)");
+
+
   return (
-    <div className="relative h-screen w-screen">
+    <div className={"h-screen w-screen"}>
       <div className="flex flex-col">
-        <div id="map" className="w-full h-full absolute top-0 left-0 z-10">
+        <div id="map" className="w-full h-full z-10">
           <LeafletMap />
         </div>
 
-        <div className="z-20 lg:ml-10 flex lg:flex-row-reverse lg:absolute lg:left-20 lg:flex-grow lg:items-center lg:mt-5">
+        <div className={largeScreen ? "z-20 ml-10 flex flex-row-reverse absolute left-20 flex-grow items-center gap-3" : 'z-20 '}>
           
           <Chip.Group multiple>
-            <div className="flex flex-row gap-2 mt-4 lg:mt-0 lg:mx-0 mx-5 overflow-x-auto">
+            <div className={largeScreen ? 'flex flex-row gap-2 ':'flex flex-row gap-2 overflow-x-auto w-80 mx-3 mt-4 absolute top-0'}>
               <Chips num="1" icono={<MdPets />} servicio="Veterinaria" />
               <Chips num="2" icono={<BsScissors />} servicio="Peluquería" />
               {/* <Chips num="3" icono={<GiDogHouse />} servicio="Refugio" /> */}
-              <Chips num="4" icono={<BiBone />} servicio="PetShop" />
-              <Chips num="5" icono={<BsHouseHeartFill />} servicio="Guardería" />
-              <Chips num="6" icono={<MdSpa />} servicio="Spa" />
-              <Chips num="7" icono={<MdLocalCafe />} servicio="Cafes" />
+              <Chips num="3" icono={<BiBone />} servicio="PetShop" />
+              <Chips num="4" icono={<BsHouseHeartFill />} servicio="Guardería" />
+              <Chips num="5" icono={<MdSpa />} servicio="Spa" />
+              <Chips num="6" icono={<MdLocalCafe />} servicio="Cafes" />
             </div>
           </Chip.Group>
 
-          <div className="flex justify-center">
-            <div className="lg:relative absolute bottom-[5%] right-[20%] group">    
-              <input
-                type="text"
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
-                placeholder="Buscar..."
-                className="px-4 md:w-2 py-1 rounded-full flex items-end border-2 outline-none focus:border-orange-400 md:group-hover:w-56 md:focus:w-56 md:transition-all md:duration-1000"
-              />
-              <button
-                onClick={handleSearch}
-                className="absolute top-0 right-0 flex items-center justify-center h-full w-9 bg-orange-400 text-white rounded-full"
-              >
-                <FaSearchLocation />
-              </button>
+          <div className={largeScreen ?  '' : 'absolute bottom-0 w-full' }>
+            <div className='flex items-center justify-center bg-gray-200 h-24 md:bg-transparent rounded-t-2xl w-auto md:rounded-none z-20 group'>
+              <div className="relative">
+                <input
+                  type="text"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  placeholder="Buscar..."
+                  className="px-4 md:w-2 py-1 w-56 rounded-full border-2 focus:w-56 outline-none focus:border-orange-400 md:group-hover:w-56 transition-all duration-1000"
+                />
+                <button
+                  onClick={handleSearch}
+                  className={largeScreen ? "absolute top-0 right-0 flex items-center justify-center h-full w-9 bg-orange-400 text-white rounded-full" : "hidden"}
+                >
+                  <FaSearchLocation />
+                </button>
+                
+              </div>
             </div>
           </div>
+          
         </div>
-        <div className='md:absolute md:bg-white md:rounded md:h-auto md:w-auto md:top-[30%] md:drop-shadow-xl md:px-2 md:ml-3  md:z-20'>
-          <div className='md:flex md:flex-col md:gap-24 md:mt-3'>
-            <div className='flex flex-col gap-3'>
-              <button className='bg-slate-300 rounded h-6 w-6 flex justify-center p-1 items-center'></button>
-              <button className='bg-slate-300 rounded h-6 w-6 flex justify-center items-center'></button>
-              <button className='bg-slate-300 rounded h-6 w-6 flex justify-center items-center'></button>
-              <button className='bg-slate-300 rounded h-6 w-6 flex justify-center items-center'></button>  
-            </div>
-            <div>            
-              <button className='bg-slate-300 rounded-full h-6 w-6 mb-3 flex justify-center items-center'></button>
-            </div>
+        <div className={largeScreen ? 'absolute top-[30%] bg-white rounded h-auto  shadow-xl px-2 ml-3 z-20' : 'hidden'}>
+              <div className='md:flex md:flex-col md:gap-24 md:mt-3'>
+                <div className='flex flex-col gap-3'>
+                  <button className='bg-slate-300 rounded h-6 w-6 flex justify-center p-1 items-center'></button>
+                  <button className='bg-slate-300 rounded h-6 w-6 flex justify-center items-center'></button>
+                  <button className='bg-slate-300 rounded h-6 w-6 flex justify-center items-center'></button>
+                  <button className='bg-slate-300 rounded h-6 w-6 flex justify-center items-center'></button>  
+                </div>
+                <div>            
+                  <button className='bg-slate-300 rounded-full h-6 w-6 mb-3 flex justify-center items-center'></button>
+                </div>
+              </div>
           </div>
-        </div>
-        {/* <div className="-top-5 md:hidden">
-          <EmailForm/>
-        </div> */}
       </div>
       
 
