07fe7c83078feea76c0085a8180dafca7a9aaaf8
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-07-05 10:45:31 -0300

    fix errors

--- src/pages/Map/LeafletMap.tsx
+++ src/pages/Map/LeafletMap.tsx
@@ -1,65 +1,71 @@
 import React, { useEffect, useRef } from 'react';
-import L from 'leaflet';
 import markerIcon from '../../../public/marcador.png';
+import dynamic from 'next/dynamic';
 
-const myIcon = L.icon({
-  iconUrl: 'https://play-lh.googleusercontent.com/G5Z7H6eE-n5tSuCszYbf_SZHRAIEmB-zLzZk_DGOMKsf7um_thkZS4QbXcMn6Tql8nVI',
-  // iconUrl: markerIcon.toString(),
-  iconSize: [40, 40],
-});
-
-const LeafletMap: React.FC = () => {
-  const mapRef = useRef<L.Map | null>(null);
+const LeafletMap: React.FC = () => {  const mapRef = useRef<L.Map | null>(null);
 
   useEffect(() => {
-    const map = L.map('map').setView([-36.5039461, -63.8486787], 5); // Argentina
-    mapRef.current = map;
-
-    const bounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180)); //Limites del mapa
-    map.setMaxBounds(bounds);
-
-    map.on('drag', () => {
-      map.panInsideBounds(bounds, { animate: false }); //No permite que se arrastre el mapa fuera de los limites
-    });
-
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
-      minZoom: 3,
-      maxZoom: 20,
-    }).addTo(map);
-
-    function addMarker(lat: number, lng: number, name: string, description: string, myImage: string) { // Agrega un marcador al mapa
-      if (mapRef.current) {
-        const marker = L.marker([lat, lng], { icon: myIcon }).addTo(mapRef.current);
-
-        const content = document.createElement('div');
-        content.innerHTML = `<b>${name}</b><br>${description}<br>${myImage ? `<img src=${myImage} />` : ''}`;
-        content.style.textAlign = 'center';
-
-        const popup = marker.bindPopup(content);
-
-        marker.on('mouseover', function () {
-          popup.openPopup();
+    import('leaflet').then((L) => {
+      if (typeof window !== 'undefined') {
+        const myIcon = L.icon({
+          iconUrl: markerIcon.toString(),
+          iconSize: [40, 40],
         });
 
-        marker.on('mouseout', function () {
-          popup.closePopup();
+        const map = L.map('map').setView([-36.5039461, -63.8486787], 5); // Argentina
+        mapRef.current = map;
+
+        const bounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180)); // Map boundaries
+        map.setMaxBounds(bounds);
+
+        map.on('drag', () => {
+          map.panInsideBounds(bounds, { animate: false }); // Prevent dragging the map outside the boundaries
         });
+
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          subdomains: 'abcd',
+          minZoom: 3,
+          maxZoom: 20,
+        }).addTo(map);
+
+        function addMarker(lat: number, lng: number, name: string, description: string, myImage: string) {
+          if (mapRef.current) {
+            const marker = L.marker([lat, lng], { icon: myIcon }).addTo(mapRef.current);
+
+            const content = document.createElement('div');
+            content.innerHTML = `<b>${name}</b><br>${description}<br>${myImage ? `<img src=${myImage} />` : ''}`;
+            content.style.textAlign = 'center';
+
+            const popup = marker.bindPopup(content);
+
+            marker.on('mouseover', function () {
+              popup.openPopup();
+            });
+
+            marker.on('mouseout', function () {
+              popup.closePopup();
+            });
+          }
+        }
+
+        addMarker(
+          -34.5499958,
+          -58.454212,
+          'ORT',
+          'La sede de PAWPAL',
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVJARrtwWfIeKn_D67r5yTFqvvy6X_FvlHloU23f1UGzvAvsIMDKLl92xO2qhIllEnbU4&usqp=CAU'
+        );
       }
-    }
-
-    addMarker(-34.5499958, -58.454212, 'ORT', 'La sede de PAWPAL', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVJARrtwWfIeKn_D67r5yTFqvvy6X_FvlHloU23f1UGzvAvsIMDKLl92xO2qhIllEnbU4&usqp=CAU');
-
-    return () => {
-      map.remove();
-    };
+    }).catch((error) => {
+      console.log('Error loading Leaflet:', error);
+    });
   }, []);
 
-  useEffect(() => { //Centra el mapa en la ubicacion del usuario
-    if (navigator.geolocation && mapRef.current) { 
-      navigator.geolocation.getCurrentPosition( 
+  useEffect(() => {
+    if (typeof window !== 'undefined' && navigator.geolocation && mapRef.current) {
+      navigator.geolocation.getCurrentPosition(
         function (position) {
           const { latitude, longitude } = position.coords;
           const map = mapRef.current;
--- src/pages/Map/mapFolderTs/mapIndex.ts
+++
@@ -1,9 +0,0 @@
-import { tileLayer, map } from 'leaflet';
-
-const mymap = map('map').setView([-34.5695195, -58.4468003], 12);
-
-tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 20
-}).addTo(mymap);
