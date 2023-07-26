b94b9cfed1b756f71f2b40bc5850a400688308d6
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-07-25 22:44:49 -0300

    minorUpdate

--- src/pages/publicaciones.tsx
+++ src/pages/publicaciones.tsx
@@ -15,12 +15,12 @@ const Publicaciones = () => {
   return (
     <div>
         <div className="h-auto flex items-center flex-col justify-start mt-10">
-        <h1 className="mb-10">Publicaciones</h1>
-        <div>
-        <CustomizedTabs value={value} onChange={handleChange} />
-        {value === 0 && <Encontrados />} 
-        {value === 1 && <Perdidos />} 
-        </div>
+            <h1 className="mb-10 font-semibold text-xl font-mono text-pink-400">Publicaciones</h1>
+            <div>
+                <CustomizedTabs value={value} onChange={handleChange} />
+                {value === 0 && <Encontrados />} 
+                {value === 1 && <Perdidos />} 
+            </div>
         </div>
         <footer><Navigation></Navigation></footer>
 
