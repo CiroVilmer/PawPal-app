5c1e3a5c042590a047a9937a8825cc9b5a27c987
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-07-26 18:12:26 -0300

    minorTabChanges

--- src/pages/publicaciones.tsx
+++ src/pages/publicaciones.tsx
@@ -3,11 +3,12 @@ import CustomizedTabs from './Components/tabs';
 import Encontrados from './encontrados'; // Import Encontrados component
 import Perdidos from './perdidos'; // Import Perdidos component
 import Navigation from './Components/navigation';
+import Link from 'next/link';
 
 const Publicaciones = () => {
   const [value, setValue] = useState<number>(0);
 
-  // Function to update the selected tab value
+  // Actualizar el valor de la tab seleccionada
   const handleChange = (newValue: number) => {
     setValue(newValue);
   };
@@ -17,9 +18,8 @@ const Publicaciones = () => {
         <div className="h-auto flex items-center flex-col justify-start mt-10">
             <h1 className="mb-10 font-semibold text-xl font-mono text-pink-400">Publicaciones</h1>
             <div>
-                <CustomizedTabs value={value} onChange={handleChange} />
-                {/* {value === 0 && <Encontrados />} 
-                {value === 1 && <Perdidos />}  */}
+              <CustomizedTabs value={value} onChange={handleChange} />
+              {value === 0 ? <Perdidos /> : <Encontrados />}            
             </div>
         </div>
         <footer><Navigation></Navigation></footer>
