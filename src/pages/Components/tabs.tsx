a51396409ab14c97cd2180cf840e477a0404e6d0
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-07-26 18:25:17 -0300

    .

--- src/pages/Components/tabs.tsx
+++ src/pages/Components/tabs.tsx
@@ -1,63 +1,63 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { styled } from '@mui/material/styles';
+// import React from 'react';
+// import Box from '@mui/material/Box';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import { styled } from '@mui/material/styles';
 
-interface StyledTabsProps {
-  children?: React.ReactNode;
-  value: number;
-  onChange: (event: React.SyntheticEvent<Element>, newValue: number) => void;
-}
+// interface StyledTabsProps {
+//   children?: React.ReactNode;
+//   value: number;
+//   onChange: (event: React.SyntheticEvent<Element>, newValue: number) => void;
+// }
 
-const StyledTabs = styled((props: StyledTabsProps) => (
-  <Tabs
-    {...props}
-    TabIndicatorProps={{ children: <span className=" max-w-[110px] bg-green-400 w-full" /> }}
-  />
-))({
-  '& .MuiTabs-indicator': {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-  },
-});
+// const StyledTabs = styled((props: StyledTabsProps) => (
+//   <Tabs
+//     {...props}
+//     TabIndicatorProps={{ children: <span className=" max-w-[110px] bg-green-400 w-full" /> }}
+//   />
+// ))({
+//   '& .MuiTabs-indicator': {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     backgroundColor: 'transparent',
+//   },
+// });
 
-interface StyledTabProps {
-  label: string;
-  onClick?: () => void;
-}
+// interface StyledTabProps {
+//   label: string;
+//   onClick?: () => void;
+// }
 
-const StyledTab = styled((props: StyledTabProps) => (<Tab disableRipple {...props} />))(() => ({
-  textTransform: 'none',
-  color: '#000000', '&.Mui-selected': { color: '#000000',},
-}));
+// const StyledTab = styled((props: StyledTabProps) => (<Tab disableRipple {...props} />))(() => ({
+//   textTransform: 'none',
+//   color: '#000000', '&.Mui-selected': { color: '#000000',},
+// }));
 
-interface TabsProps {
-  onChange: (value: number) => void;
-  value: number;
-}
+// interface TabsProps {
+//   onChange: (value: number) => void;
+//   value: number;
+// }
 
-const CustomizedTabs = ({ onChange, value }: TabsProps) => {
+// const CustomizedTabs = ({ onChange, value }: TabsProps) => {
 
-   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-     onChange(newValue);
-   };
+//    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+//      onChange(newValue);
+//    };
   
-   return (
-     <Box sx={{ width: '100%'}}>
-       <Box>
-         <StyledTabs
-           value={value}
-           onChange={handleChange}
-         >
-           <StyledTab label="Encontrados"/>
-           <StyledTab label="Perdidos" />  
-         </StyledTabs>
-         <Box sx={{ p: 3 }} />
-       </Box>
-     </Box>
-   );
-}
+//    return (
+//      <Box sx={{ width: '100%'}}>
+//        <Box>
+//          <StyledTabs
+//            value={value}
+//            onChange={handleChange}
+//          >
+//            <StyledTab label="Encontrados"/>
+//            <StyledTab label="Perdidos" />  
+//          </StyledTabs>
+//          <Box sx={{ p: 3 }} />
+//        </Box>
+//      </Box>
+//    );
+// }
 
-export default CustomizedTabs;
+// export default CustomizedTabs;
--- src/pages/publicaciones.tsx
+++ src/pages/publicaciones.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import CustomizedTabs from './Components/tabs';
-import Encontrados from './encontrados'; // Import Encontrados component
-import Perdidos from './perdidos'; // Import Perdidos component
-import Navigation from './Components/navigation';
-import Link from 'next/link';
+// import React, { useState } from 'react';
+// import CustomizedTabs from './Components/tabs';
+// import Encontrados from './encontrados'; // Import Encontrados component
+// import Perdidos from './perdidos'; // Import Perdidos component
+// import Navigation from './Components/navigation';
+// import Link from 'next/link';
 
-const Publicaciones = () => {
-  const [value, setValue] = useState<number>(0);
+// const Publicaciones = () => {
+//   const [value, setValue] = useState<number>(0);
 
-  // Actualizar el valor de la tab seleccionada
-  const handleChange = (newValue: number) => {
-    setValue(newValue);
-  };
+//   // Actualizar el valor de la tab seleccionada
+//   const handleChange = (newValue: number) => {
+//     setValue(newValue);
+//   };
 
-  return (
-    <div>
-        <div className="h-auto flex items-center flex-col justify-start mt-10">
-            <h1 className="mb-10 font-semibold text-xl font-mono text-pink-400">Publicaciones</h1>
-            <div>
-              <CustomizedTabs value={value} onChange={handleChange} />
-              {value === 0 ? <Perdidos /> : <Encontrados />}            
-            </div>
-        </div>
-        <footer><Navigation></Navigation></footer>
+//   return (
+//     <div>
+//         <div className="h-auto flex items-center flex-col justify-start mt-10">
+//             <h1 className="mb-10 font-semibold text-xl font-mono text-pink-400">Publicaciones</h1>
+//             <div>
+//               <CustomizedTabs value={value} onChange={handleChange} />
+//               {value === 0 ? <Perdidos /> : <Encontrados />}            
+//             </div>
+//         </div>
+//         <footer><Navigation></Navigation></footer>
 
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default Publicaciones;
+// export default Publicaciones;
