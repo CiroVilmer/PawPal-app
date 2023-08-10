8ff602af8d3434fa333ef09c178330b6b59d2f97
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-08-10 10:26:07 -0300

    Update createAccount.tsx

--- src/pages/createAccount.tsx
+++ src/pages/createAccount.tsx
@@ -119,14 +119,16 @@ export function CreateAccount() {
                 />
               </Input.Wrapper>
             </div>
-          </Form>
-          </Formik> 
+
           <div>
           <button className="w-full bg-orange-400 text-white rounded-xl py-2 mb-1 hover:bg-orange-500 focus:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-200 focus:ring-offset-1 transform transition duration-400 ease-in active:scale-[.98]" type='submit'>
             Crear cuenta
           </button>
-      
+    
+
           </div>
+          </Form>
+          </Formik> 
           <Toaster
           position="top-center"
           reverseOrder={false}
