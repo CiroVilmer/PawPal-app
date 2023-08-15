05ed628dc9a8d1b9b771226c654f6c4ea5ad0ee4
Luciano Manuel Del Arco <47138072@est.ort.edu.ar>
2023-08-15 13:24:42 -0300

    updatedNav

--- src/pages/Components/navigation.tsx
+++ src/pages/Components/navigation.tsx
@@ -37,7 +37,7 @@ const Navigation = () => {
     <div className="">
 
       { mediumScreen ? <a className='hidden'></a> : <div className={`bg-white w-32 h-auto absolute -bottom-16 left-2 ${isOpen ? "-translate-y-32 duration-700" : "translate-y-24 duration-1000"}`}>
-        <div className={ "flex flex-col gap-2 items-start ml-3 py-2"}>
+        <div className={`${isOpen ? "flex flex-col gap-2 items-start ml-3 py-2" : "hidden"} `}>
           
             <Link href='/contacto'>
                 <div className="flex flex-row gap-2 text-gray-400 items-center">
