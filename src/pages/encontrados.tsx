307115877b6fbc1b8bcbaf2c74ebdbb3f4ad943b
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-10-17 22:46:52 -0300

    Update encontrados.tsx

--- src/pages/encontrados.tsx
+++ src/pages/encontrados.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import PostCard from './Components/publicationCard'
 import {useMediaQuery} from "@mantine/hooks";
+import { api } from '~/utils/api';
 
 
 const Encontrados: React.FC = () : JSX.Element => {
+
+    const activePosts = api.post.getPosts.useQuery({});
+
+    for (const post of activePosts?.data || []) {
+        console.log(post);
+
+        const title = post.title;
+        const location = post.location;
+        const description = post.description;
+    }
+
     const mediumScreen = useMediaQuery('(min-width: 768px)');
     return (
         <div className={`background ${mediumScreen ? 'grid  md:grid-cols-2 lg:grid-cols-3 gap-y-2 gap-x-6 overflow-y-auto h-[730px] w-auto ml-16 mr-10' : 'flex flex-col ml-4 overflow-y-auto h-[480px]'}`}>
