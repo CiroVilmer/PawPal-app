f8cff0d1680abe6227ad192db5a85a8066e01801
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-11-14 14:22:56 -0300

    Update imageupload.tsx

--- src/pages/imageupload.tsx
+++ src/pages/imageupload.tsx
@@ -1,6 +1,5 @@
 import { env } from "process";
-const cloudinary = require('cloudinary').v2;
-
+import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
     cloudname: process.env.CLOUDINARY_USER_NAME,
@@ -12,15 +11,15 @@ cloudinary.config({
 
 console.log(cloudinary.config());
 
-const Formulario = () => {
-    return(
-        <form>
-            <label  className="flex flex-col mb-4  items-center justify-center md:w-[450px] w-[290px] h-44 md:h-44 border-2 border-gray-300 px-4 rounded-lg cursor-pointer bg-gray-200">
-                <input id="dropzone-file" type="file" className="hidden" />
-            </label>
-            <button type="submit" className="w-[220px] y-12 ">enviar</button>
-        </form>
-    )
-}
+// const Formulario = () => {
+//     return(
+//         <form>
+//             <label  className="flex flex-col mb-4  items-center justify-center md:w-[450px] w-[290px] h-44 md:h-44 border-2 border-gray-300 px-4 rounded-lg cursor-pointer bg-gray-200">
+//                 <input id="dropzone-file" type="file" className="hidden" />
+//             </label>
+//             <button type="submit" className="w-[220px] y-12 ">enviar</button>
+//         </form>
+//     )
+// }
 
-export default Formulario;
+// export default Formulario;
