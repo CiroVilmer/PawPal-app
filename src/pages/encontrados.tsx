d3143be84d17cfca478cde7c85aa211b32d74778
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-11-14 10:30:59 -0300

    Update encontrados.tsx

--- src/pages/encontrados.tsx
+++ src/pages/encontrados.tsx
@@ -8,7 +8,9 @@ const Encontrados: React.FC = () : JSX.Element => {
 
     const activePosts = api.post.getPosts.useQuery({});
 
-    
+
+    const mediumScreen = useMediaQuery('(min-width: 768px)');
+
     activePosts?.data?.map((post) => {
         console.log(post);
 
@@ -22,18 +24,13 @@ const Encontrados: React.FC = () : JSX.Element => {
         )
 
     });
-
-
-    const mediumScreen = useMediaQuery('(min-width: 768px)');
+    
     return (
         <div className={`background ${mediumScreen ? 'grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-2 gap-x-6 overflow-y-auto h-[480px] w-auto ml-16 mr-10' : 'flex flex-col ml-4 overflow-y-auto h-[480px]'}`}>
             <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!' key=''/>
-            <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!' key=''/>
-            <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!' key=''/>
-            <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!'key=''/>
-            <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!'key=''/>
-            <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!' key=''/>
-            <PostCard img='./hotdog.jpg' name='Pancho' ubication='Los Pinos' description='Panchito, el salchi, se mandó el viernes a pique para fletar una paloma. Chiquito pero picante, dejó a todos pillos. Ahora anda desaparecido en acción, dejando a su banda buscándolo por cada rincón de la city. ¡A cruzar los dedos para que este fierrito vuelva a casa!' key=''/>
+            {activePosts?.data?.map((post) => (
+                <PostCard img='./hotdog.jpg' name={post.title} ubication={post.location} description={post.description === null ? "" : post.description } key={post.id} />
+            ))}
         </div>
     )
 }
