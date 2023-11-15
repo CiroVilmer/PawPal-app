2f40b7d1f53448e3aa75cce8f1e82f25a9f72cf0
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-11-15 08:41:18 -0300

    Update imageupload.tsx

--- src/pages/imageupload.tsx
+++ src/pages/imageupload.tsx
@@ -1,15 +1,15 @@
 import { imageUploader } from "~/utils/imageUploade";
 
 
-// const Formulario = () => {
-//     return(
-//         <form>
-//             <label  className="flex flex-col mb-4  items-center justify-center md:w-[450px] w-[290px] h-44 md:h-44 border-2 border-gray-300 px-4 rounded-lg cursor-pointer bg-gray-200">
-//                 <input id="dropzone-file" type="file" className="hidden" />
-//             </label>
-//             <button type="submit" className="w-[220px] y-12 ">enviar</button>
-//         </form>
-//     )
-// }
+const Formulario : React.FC = () => {
+    return(
+        <form>
+            <label  className="flex flex-col mb-4  items-center justify-center md:w-[450px] w-[290px] h-44 md:h-44 border-2 border-gray-300 px-4 rounded-lg cursor-pointer bg-gray-200">
+                <input id="dropzone-file" type="file" className="hidden" />
+            </label>
+            <button type="submit" className="w-[220px] y-12 ">enviar</button>
+        </form>
+    )
+}
 
-// export default Formulario;
+export default Formulario;
