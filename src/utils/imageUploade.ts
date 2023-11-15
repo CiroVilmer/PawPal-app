7fdae03426412d9ee4b0a6b3e33c19e8b7d34087
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-11-15 08:37:24 -0300

    Update imageUploade.ts

--- src/utils/imageUploade.ts
+++ src/utils/imageUploade.ts
@@ -6,14 +6,22 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const imageUploader = async (file: any) => {
+
+export const imageUploader = async (file: File) => {
     try {
-        const res = await cloudinary.uploader.upload(file, {
+        // Convert File object to a temporary URL
+        const url = URL.createObjectURL(file);
+
+        // Upload using temporary URL
+        const res = await cloudinary.uploader.upload(url, {
             folder: 'test',
         });
+
+        // Revoke temporary URL after upload
+        URL.revokeObjectURL(url);
+
         return res.secure_url;
     } catch (error) {
         console.log(error);
     }
 }
-
