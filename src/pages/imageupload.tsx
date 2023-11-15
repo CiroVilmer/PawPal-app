11ca436783a8fbdb00230219c05062ede37c3ad2
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-11-15 09:03:59 -0300

    created imageuplod

--- src/pages/imageupload.tsx
+++ src/pages/imageupload.tsx
@@ -1,15 +1,47 @@
+import React, { useState } from "react";
 import { imageUploader } from "~/utils/imageUploade";
 
 
 const Formulario : React.FC = () => {
-    return(
-        <form>
-            <label  className="flex flex-col mb-4  items-center justify-center md:w-[450px] w-[290px] h-44 md:h-44 border-2 border-gray-300 px-4 rounded-lg cursor-pointer bg-gray-200">
-                <input id="dropzone-file" type="file" className="hidden" />
-            </label>
-            <button type="submit" className="w-[220px] y-12 ">enviar</button>
-        </form>
-    )
-}
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (files && files.length > 0) {
+      setSelectedFile(files[0]!);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (selectedFile) {
+      try {
+        const imageUrl = await imageUploader(selectedFile);
+        console.log('Image uploaded:', imageUrl);
+        // You can handle the uploaded URL as needed (e.g., store in state, send to server, etc.)
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label className="flex flex-col mb-4 items-center justify-center md:w-[450px] w-[290px] h-44 md:h-44 border-2 border-gray-300 px-4 rounded-lg cursor-pointer bg-gray-200">
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+      </label>
+      <button type="submit" className="w-[220px] y-12">
+        Enviar
+      </button>
+    </form>
+  );
+};
 
 export default Formulario;
--- src/utils/imageUploade.ts
+++ src/utils/imageUploade.ts
@@ -1,4 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
+import App from "next/app";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
