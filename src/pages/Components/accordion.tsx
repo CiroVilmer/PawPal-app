ccb2e24a4c6093b0910165f31561ea8afb0a9a58
Luciano Manuel Del Arco <47138072@est.ort.edu.ar>
2023-11-15 14:58:05 -0300

    minorFix

--- src/pages/Components/accordion.tsx
+++ src/pages/Components/accordion.tsx
@@ -1,28 +1,21 @@
-import {Accordion} from '@mantine/core';
+import { Accordion } from '@mantine/core';
 
-interface PosteosForoProps{
-  title: string,
-  description: string,
-
+interface PosteosForoProps {
+  title: string;
+  description: string;
 }
 
-
-const AccordionForo : React.FC<PosteosForoProps> = ({title, description}) => {
-    // function body goes here
-    
-    
-
-    return (
-        <div className='w-[340px] md:w-[940px]'>
-            <Accordion variant='separated' radius={"md"} className=''>
-              <Accordion.Item key={title} value={title}>
-                <Accordion.Control >{title}</Accordion.Control>
-                <Accordion.Panel>{description}</Accordion.Panel>
-              </Accordion.Item>
-            </Accordion>
-        </div>
-    )
-      
+const AccordionForo: React.FC<PosteosForoProps> = ({ title, description }) => {
+  return (
+    <div className='w-[340px] md:w-[940px]'>
+      <Accordion variant='separated' radius='md'>
+        <Accordion.Item value={title}>
+          <Accordion.Control>{title}</Accordion.Control>
+          <Accordion.Panel>{description}</Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
 };
 
 export default AccordionForo;
