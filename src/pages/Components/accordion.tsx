7874870826307306acf839b0f79dd9e6df7b9d5d
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-11-16 13:13:38 -0300

    updatedForoPost

--- src/pages/Components/accordion.tsx
+++ src/pages/Components/accordion.tsx
@@ -10,11 +10,11 @@ const AccordionForo: React.FC<PosteosForoProps> = ({ title, description }) => {
   
   return (
     <div className='w-[340px] md:w-[940px]'>
-      <Card>
+      <Card color='orange' withBorder className='shadow-lg rounded-lg p-8 mb-2'>
         <Card.Section>
           <Box>
-            <Text size="xl" weight={500}>{title}</Text>
-            <Text size="sm" weight={300}>{description}</Text>
+            <Text size="xl" weight={700} className='text-[#144F60]'>{title}</Text>
+            <Text size="sm" weight={450}>{description}</Text>
           </Box>
         </Card.Section>
       </Card>
