ab3380d839bd4c9dc0895c8eff3f8f3a539a8213
Luciano Del Arco <47138072@est.ort.edu.ar>
2023-11-17 14:25:14 -0300

    minorUpdate

--- src/pages/foro.tsx
+++ src/pages/foro.tsx
@@ -13,14 +13,15 @@ const Refugios: React.FC = () : JSX.Element => {
     const activePosts = api.post.getForumPosts.useQuery({});
 
 
-    activePosts?.data?.map((post: { id: string; title: string | null; description: string | null; }) => {
+    activePosts?.data?.map((post: { id: string; title: string | null; description: string | null; type: string | null }) => {
         console.log(post);
 
         const id = post.id;
         const title = post.title;
         const descriptionPost = post.description;
+        const type = post.type;
         return (
-        <ForoCard title={title === null ? "" : title } description={descriptionPost === null ? "" : descriptionPost } key={id} />
+        <ForoCard type={type === null ? "otro" : type} title={title === null ? "" : title } description={descriptionPost === null ? "" : descriptionPost } key={id} />
         )
 
     });
@@ -30,10 +31,11 @@ const Refugios: React.FC = () : JSX.Element => {
             <div className="flex-col justify-start mt-10">
                 <h1 className="text-7xl font-semibold text-orange-400 text-center mb-16">Foro</h1>
                 <section className="overflow-y-auto h-[400px] flex flex-col-reverse">
-                {activePosts?.data?.map((post: { title: string | null; description: string | null; id: string; }) => (
-                <ForoCard title={post.title === null ? "" : post.title } description={post.description === null ? "" : post.description } key={post.id} />
+                {activePosts?.data?.map((post: { title: string | null; description: string | null; id: string; type: string | null}) => (
+                <ForoCard type={post.type === null ? "otro" : post.type} title={post.title === null ? "" : post.title } description={post.description === null ? "" : post.description } key={post.id} />
                 ))}
                 </section>
+                <ForoCard title="Titulo" description="Descripcion" type='Pregunta' key="1" />
     
             </div>
             <ModalForo/>
