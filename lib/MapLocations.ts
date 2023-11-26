0aff9768fd61a6b4c73a678d2bb35a60025f39e7
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2023-11-26 17:13:20 -0300

    Update MapLocations.ts

--- lib/MapLocations.ts
+++ lib/MapLocations.ts
@@ -38,7 +38,7 @@ export const Markerlocations = [
   { lat: -34.5550092, lng: -58.4844013, name: 'Doctors House ...', description: 'Descripcion', category: 'Home' }
 ]
 
-export let Circlelocations = [
+export const Circlelocations = [
     { lat: -34.5497574, lng: -58.4541175, radius: 500, color: 'yellow', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden cachorro', category: 'LostDog' },
     { lat: -34.5366564, lng: -58.4548797, radius: 500, color: 'red', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden adulto', category: 'LostDog' },
     { lat: -34.5626489, lng: -58.4528739, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
