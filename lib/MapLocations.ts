ad96ff0460945f9b10fa39167fb67ad0c1415c40
FacuSalco <facusalco26@gmail.com>
2023-11-26 17:44:23 -0300

    Update MapLocations.ts

--- lib/MapLocations.ts
+++ lib/MapLocations.ts
@@ -41,12 +41,5 @@ export const Markerlocations = [
 export const Circlelocations = [
     { lat: -34.5497574, lng: -58.4541175, radius: 500, color: 'yellow', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden cachorro', category: 'LostDog' },
     { lat: -34.5366564, lng: -58.4548797, radius: 500, color: 'red', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden adulto', category: 'LostDog' },
-    { lat: -34.5550092, lng: -58.4844013, radius: 500, color: 'orange', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden cachorro', category: 'LostDog' },
     { lat: -34.5626489, lng: -58.4528739, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
-    { lat: -34.5626489, lng: -58.5528739, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
-    { lat: -34.5836489, lng: -58.4528709, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
-    { lat: -34.3126489, lng: -58.4528779, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
-    { lat: -34.5626569, lng: -58.4528729, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
-    { lat: -34.5622489, lng: -58.2528739, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
-    { lat: -34.5622389, lng: -58.0528739, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
 ];
