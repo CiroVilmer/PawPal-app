9b9a98b5a91f491cf19466cf6eb11cd30923b1e9
FacuSalco <facusalco26@gmail.com>
2023-11-26 17:50:13 -0300

    Update MapLocations.ts

--- lib/MapLocations.ts
+++ lib/MapLocations.ts
@@ -42,4 +42,6 @@ export const Circlelocations = [
     { lat: -34.5497574, lng: -58.4541175, radius: 500, color: 'yellow', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden cachorro', category: 'LostDog' },
     { lat: -34.5366564, lng: -58.4548797, radius: 500, color: 'red', name: 'Perro perdido', description: 'Se perdio Pancho, es un golden adulto', category: 'LostDog' },
     { lat: -34.5626489, lng: -58.4528739, radius: 500, color: 'green', name: 'Perro perdido', description: 'Se perdio Pancho, es un galgo adulto', category: 'LostDog' },
+    { lat: -34.49470022448979, lng: -58.501759102040815, radius: 500, color: 'orange', name: 'Perro perdido', description: 'CiroVilmer', category: 'LostDog' },
+
 ];
